a3387c48b3400557272d5096e773731af6e31169
Zakiyyah07 <143916732+Zakiyyah07@users.noreply.github.com>
2023-09-14 18:54:47 +0800

    Delete H071231001/Praktikum-1 directory
    menghapus direktori

--- H071231001/Praktikum-1/TP1_4_H071231001.py
+++
@@ -1,7 +0,0 @@
-print("pengujian jenis karakter\n------")
-
-k = input("karakter = ")
-
-print("huruf kapital?",k.isupper())
-print("huruf kecil?",k.islower())
-print("angka?",k.isdigit())
--- H071231001/Praktikum-1/TP1_5_H071231001.py
+++
@@ -1,9 +0,0 @@
-print("konversi jam ke detik:")
-
-x = int(input("masukkan jumlah detik:"))
-
-jam = x//3600
-menit = (x%3600)//60
-detik = x%60
-
-print(f"{jam:02d}:{menit:02d}:{detik:02d}")
