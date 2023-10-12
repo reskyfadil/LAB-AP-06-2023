b841b593d304b444509a8261858f022d7405060f
reskyfadil <72343508+reskyfadil@users.noreply.github.com>
2023-10-12 22:46:36 +0800

    Delete TP4_2_H071231034.py

--- TP4_2_H071231034.py
+++
@@ -1,8 +0,0 @@
-def cek_palindrom(kata):
-    if kata == kata[::-1]:
-        return "Palindrom"
-    else:
-        return "Bukan Palindrom"
-
-kata = input("Masukkan kata: ")
-print(cek_palindrom(kata)) 
